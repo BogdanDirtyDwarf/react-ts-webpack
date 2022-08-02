7eb13a289f5883d8249d13cec9734849d5e9434c
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2022-08-02 23:48:58 +0500

    fixed bug with images

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
             },
             {
                 test: /\.(png|jpeg|svg|jpg|gif|webp)$/i,
-                use: ['file-loader']
+                type: 'asset/resource'
             }
         ]
     }
