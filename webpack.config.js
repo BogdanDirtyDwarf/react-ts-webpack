7b872f3f6d3f8f2c7267ba90beea9e85d27b09ce
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2022-08-03 01:28:09 +0500

    Added mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -36,13 +37,17 @@ module.exports = {
                         to: path.resolve(__dirname, 'build')
                     }
                 ]
-            })
+            }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }
+        )
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
             {
                 test: /\.(png|jpeg|svg|jpg|gif|webp)$/i,
